d1b4280fc4287b73a4db36f30d3b6a609ebd3342
Batmunkh Namshir <namshir17@gmail.com>
2022-03-24 15:41:22 -0700

    added routes for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,56 @@
 class CommentsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :destroy_invalid
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+    def index
+        comments = Comment.all
+        render json: comments
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment
+    end
+
+    def create
+        comment = Comment.create!(params_create)
+        render json: comment
+    end
+
+    # add validation
+
+
+    def destroy
+        comment = Comment.find(params{:id})
+        comment.destroy
+        head :no_content
+    end
+
+
+    def update
+        comment = Comment.find(params[:id])
+        comment.update(params_create)
+        render json: comment
+    end
+
+
+
+    private
+
+    def params_create
+        params.permit(:content)
+    end
+
+    def destroy_invalid
+        render json: {error: "Comment not found"}, status: :not_found
+    end
+
+    def render_not_found
+        render json: {errors: "comment not found"}, status: :not_found
+    end
+
+    def render_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
     resources :posts
-    resources :comments, only: []
+    resources :comments
     resources :forums, only: []
     resources :users, only: []
     
     get "/sessions", to: "sessions#index"
     get "/me", to: "users#show"
     post "/login", to: "sessions#create"
-    delete "/logout" to: "sessions#destroy"
+    # delete "/logout" to: "sessions#destroy"
   
   end
 
