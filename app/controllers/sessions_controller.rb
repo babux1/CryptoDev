adbf487aa4a9ff6560a8aabcaa489d43ef9c7451
Eric Einerson <eaeiner.gmail.co>
2022-03-24 15:58:00 -0600

    made a logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
         cookies[:cookies_hello] ||= "World"
         render json: { session: session, cookies: cookies.to_hash }
     end
+
     def create
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id 
         render json: user
     end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     resources :posts
-    resources :thread_rs, only: []
     resources :comments, only: []
     resources :forums, only: []
     resources :users, only: []
