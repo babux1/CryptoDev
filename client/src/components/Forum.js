97d261dbf3179fcab1c54b54eeb395cd7ae4028d
Mike Starr <imhs@protonmail.com>
2022-03-25 12:58:54 -0700

    Setup useEffect hook on Forum.js to render all Posts. Modified package.json file line 2 to add proxy:localhost:300, and line 16 to add start: port=4000 react-scripts start

--- client/src/components/Forum.js
+++ client/src/components/Forum.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import ForumSearch from "./ForumSearch";
 
 function Forum() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("/posts")
+      .then((r) => r.json())
+      .then((posts) => {
+        console.log(posts);
+        setPosts(posts);
+      });
+  }, []);
+
   return (
     <div className="forum">
       <h2>Solidity Forum</h2>
