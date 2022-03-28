e77147151bfdfbf6c9ec6a409ca1e9de011f95f8
Eric Einerson <eaeiner.gmail.co>
2022-03-28 11:31:09 -0600

    took out an extra import

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
 
