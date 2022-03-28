ffab98647f246f5ea95264011f88703fb92b9aab
Eric Einerson <eaeiner.gmail.co>
2022-03-28 16:47:45 -0600

    added some changes to login logic

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -15,4 +15,50 @@ function LoginPage() {
     )
 }
 
-export default LoginPage;
+// export default LoginPage;
+
+// import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+// import {Route, BrowserRouter} from "react-router-dom";
+
+// function LoginPage({ onLogin }) {
+
+// const [username, setUsername] = useState("")
+// const [password, setPassword] = useState("")
+
+//     function handleSubmit(event) {
+//         event.preventDefault();
+//         fetch("/login", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({ username }),
+//         })
+//         .then(r => r.json())
+//         .then(user => onLogin(user));
+//     }
+
+//     return (
+//         <div>
+//             <form onSubmit = {handleSubmit}>
+//                 <input 
+//                 type = "text" 
+//                 placeholder = "username"
+//                 value = {username}
+//                 onChange = { e => setUsername(e.target.value)}
+//                 />
+//                 {/* <input 
+//                 type = "text" 
+//                 placeholder = "password"
+//                 value = {password}
+//                 onChange = { e => setPassword(e.target.value)}
+//                 /> */}
+//                 <br></br>
+//                 <button>Enter</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default LoginPage;
