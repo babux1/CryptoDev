17f1b8d1602b3ae830d557c6b806dbbe84abc29c
Eric Einerson <eaeiner.gmail.co>
2022-03-28 11:23:21 -0600

    fixed routing stuff

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Nav from "./Nav";
+import {Link} from "react-router-dom";
 import Login from "./Login";
+import LoginPage from "./LoginPage"
 import Forum from "./Forum";
 import ForumsList from "./ForumsList";
 
@@ -12,6 +15,13 @@ function App() {
       <h1>CryptoDev Forum</h1>
       <div className="left-column">
         <Forum />
+      </div>
+      <div>
+      <div>
+      <Routes>
+      <Route path = "loginpage" element = {<LoginPage />}/>
+      </Routes>
+      </div>
       </div>
       {/* <div className="right-column">
         <ForumsList />
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,17 @@
 import React from "react";
+import LoginPage from "./LoginPage";
+import {Link} from "react-router-dom";
 
 function Login() {
-  return <div className="login">Login here</div>;
+
+  return (
+    <Link to="/loginpage">
+     <button type="button">
+          Login here!
+     </button>
+    </Link>
+  )
+  
 }
 
 export default Login;
